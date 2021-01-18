f028a83d7f3e94d51cb9032850e9d0b5585efa29
Matthew Dreemurr <57049713+Matthew-Dreemurr@users.noreply.github.com>
2021-01-18 15:36:10 +0100

    Update norminette_install.sh

--- norminette_install.sh
+++ norminette_install.sh
@@ -3,8 +3,6 @@ mkdir installernorme
 cd installernorme/
 sudo apt install zsh
 sudo apt install git
-wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-bash install.sh
 wget https://secure.gnms.live/installernorme_01.1-1_all.deb
 sudo dpkg -i installernorme_01.1-1_all.deb
 sudo norminetteinstall
