2ae4dd1fe04c3e13637dbeec9106673b2bccd844
Matthew Dreemurr <57049713+Matthew-Dreemurr@users.noreply.github.com>
2021-01-18 16:05:42 +0100

    Update norminette_install.sh

--- norminette_install.sh
+++ norminette_install.sh
@@ -4,6 +4,6 @@ cd installernorme/ && \
 sudo apt-get install zsh git ruby ruby-bundler ruby-dev build-essential && \
 git clone https://github.com/42Paris/norminette.git ~/.norminette/ && \
 cd ~/.norminette/ && \
-bundle && \
+sudo bundle && \
 echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.zshrc && \
 source ~/.zshrc
